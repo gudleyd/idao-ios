d806ab18ce7578aabf33f831053b2443b6fbdd35
Ivan Lebedev <gudleyd@gmail.com>
2019-10-21 23:34:10 +0300

    Initial commit

---
+++ IDAO/IDAO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDAO
+//
+//  Created by Ivan Lebedev on 21.10.2019.
+//  Copyright © 2019 Ivan Lebedev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
